aef2fd9c5461aed59b6fba2c5110b1e719ad4709
Adam Demasi <adam@hbang.com.au>
2025-05-10 23:51:40 +0930

    Add custom icon selection

---
+++ swiped./SettingsIconView.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsIconView.swift
+//  swiped.
+//
+//  Created by Adam Demasi on 10/05/2025.
+//
+
+import SwiftUI
+
+struct SettingsIconView: View {
+
+	let icons = [
+		"DefaultIcon",
+		"SFIcon"
+	]
+
+	var body: some View {
+		ScrollView {
+			LazyVGrid(
+				columns: [
+					GridItem(.fixed(70), alignment: .center),
+					GridItem(.fixed(70), alignment: .center),
+					GridItem(.fixed(70), alignment: .center),
+					GridItem(.fixed(70), alignment: .center)
+				],
+				alignment: .center) {
+					ForEach(icons, id: \.self) { icon in
+						Button(action: {
+							if icon == "DefaultIcon" {
+								UIApplication.shared.setAlternateIconName(nil)
+							} else {
+								UIApplication.shared.setAlternateIconName(icon)
+							}
+						}, label: {
+							Image(uiImage: UIImage(named: icon) ?? UIImage(systemName: "questionmark")!)
+								.frame(width: 60, height: 60)
+								.background(Color(UIColor.secondarySystemBackground))
+								.cornerRadius(12)
+								.overlay(
+									RoundedRectangle(cornerRadius: 12)
+										.stroke(Color(UIColor.separator), lineWidth: 1)
+								)
+						})
+							.padding(5)
+					}
+				}
+		}
+			.navigationTitle("Icons")
+	}
+}
+
+#Preview {
+	SettingsIconView()
+}
--- swiped./SettingsView.swift
+++ swiped./SettingsView.swift
@@ -24,58 +24,66 @@ struct SettingsView: View {
 	var showResetAlert: Bool = false
 
 	var body: some View {
-		Form {
-			Section(content: {}, header: {
-				HStack {
-					Spacer()
-					VStack(alignment: .center) {
-						Text("SWIPED")
-							.foregroundColor(.white)
-							.font(.custom("LoosExtended-Bold", size: 55))
-						+
-						Text(".")
-							.foregroundColor(.accentColor)
-							.font(.custom("LoosExtended-Bold", size: 55))
-
-						Text("Version \(version) (\(build))")
-							.font(.custom("LoosExtended-Bold", size: 18))
-					}
-					Spacer()
-				}
-					.padding(.vertical, 30)
-			})
-
-			Section {
-				Toggle(isOn: $sync) {
-					Text("Sync")
-						.font(.custom("LoosExtended-Bold", size: 16))
-				}
-			}
-
-			Section {
-				Button(action: {
-					showResetAlert = true
-				}, label: {
+		NavigationView {
+			Form {
+				Section(content: {}, header: {
 					HStack {
 						Spacer()
-						Label("Reset Database", systemImage: "xmark.bin")
-							.font(.custom("LoosExtended-Bold", size: 16))
+						VStack(alignment: .center) {
+							Text("SWIPED")
+								.foregroundColor(.white)
+								.font(.custom("LoosExtended-Bold", size: 50))
+							+
+							Text(".")
+								.foregroundColor(.accentColor)
+								.font(.custom("LoosExtended-Bold", size: 50))
+
+							Text("Version \(version) (\(build))")
+								.font(.custom("LoosExtended-Bold", size: 18))
+						}
 						Spacer()
 					}
+						.padding(.vertical, 30)
 				})
-					.foregroundColor(.red)
-			}
-		}
-			.alert("You will lose all statistics you have collected so far.", isPresented: $showResetAlert) {
-				// do stuff
-			} message: {
-				Text("farts")
-			}
 
+//				Section {
+//					Toggle(isOn: $sync) {
+//						Text("Sync")
+//							.font(.custom("LoosExtended-Bold", size: 16))
+//					}
+//				}
+
+				Section {
+					NavigationLink("Icon") {
+						SettingsIconView()
+					}
+				}
+
+				Section {
+					Button(action: {
+						showResetAlert = true
+					}, label: {
+						HStack {
+							Spacer()
+							Label("Reset Database", systemImage: "xmark.bin")
+								.font(.custom("LoosExtended-Bold", size: 16))
+							Spacer()
+						}
+					})
+						.foregroundColor(.red)
+				}
+			}
+				.alert("You will lose all statistics you have collected so far.", isPresented: $showResetAlert, actions: {
+					Button("Reset", role: .destructive) {
+						DatabaseController.shared.reset()
+					}
+					Button("Cancel", role: .cancel) {}
+				})
+		}
 	}
 
 }
 
 #Preview {
-    SettingsView()
+	SettingsView()
 }
