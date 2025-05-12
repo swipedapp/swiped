0cdeaba4fe302442e068d6bd1bcca3618c6081b7
miss australiana <57821130+fullpwn@users.noreply.github.com>
2025-05-12 16:08:26 +1000

    Added: auto.sh

---
+++ auto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+git add .
+added=$(git diff --cached --name-only --diff-filter=A | tr '\n' ' ')
+modified=$(git diff --cached --name-only --diff-filter=M | tr '\n' ' ')
+deleted=$(git diff --cached --name-only --diff-filter=D | tr '\n' ' ')
+message=""
+if [ -n "$added" ]; then
+  message="${message}Added: ${added}"
+fi
+if [ -n "$modified" ]; then
+  if [ -n "$message" ]; then
+    message="${message}\n"
+  fi
+  message="${message}Modified: ${modified}"
+fi
+if [ -n "$deleted" ]; then
+  if [ -n "$message" ]; then
+    message="${message}\n"
+  fi
+  message="${message}Deleted: ${deleted}"
+fi
+if [ -z "$message" ]; then
+  message="No Change"
+  exit 0
+fi
+
+# Write message to the commit message file
+git commit -m "$message"
