48fccd83470f01df3868b18c0ed2cd03a3a3e3fe
miss australiana <57821130+fullpwn@users.noreply.github.com>
2025-05-12 23:24:27 +1000

    Modified: swiped./InternalView.swift

--- swiped./InternalView.swift
+++ swiped./InternalView.swift
@@ -12,18 +12,21 @@ struct InternalView: View {
 	@State
 	var sync: Bool = false
 	var body: some View {
-		
+		Spacer()
+		Text("Be careful what you wish for!").font(.custom("LoosExtended-Bold", size: 16))
+		Text("Things in here are experimental, and could fail.").font(.custom("LoosExtended-Regular", size: 16))
 		Form {
 			Section {
 				NavigationLink("App Icons") {
 					SettingsIconView()
 				}
+				Toggle(isOn: $sync) {
+					Text("Disable Sync")
+						.font(.custom("LoosExtended-Regular", size: 16))
+				}
 			}
 
-			Toggle(isOn: $sync) {
-				Text("Disable Sync")
-					.font(.custom("LoosExtended-Regular", size: 16))
-			}
+			
 		}
 		}
 		
