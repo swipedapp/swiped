8e5e09c14975a9b2cb281232d623c90f2865f200
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-05-20 14:24:20 +1000

    Modified: swiped./SettingsView.swift

--- swiped./SettingsView.swift
+++ swiped./SettingsView.swift
@@ -108,13 +108,13 @@ struct SettingsView: View {
 					})
 				
 				Spacer()
-				Text(commitInfo).opacity(0.5).font(.custom("LoosExtended-Medium", size: 16))
+				
 				#if INTERNAL
 				Text("For testing only.").opacity(0.5).font(.custom("LoosExtended-Medium", size: 16))
 				#elseif DEBUG
 				Text("Internal release. Do not submit").opacity(0.5).font(.custom("LoosExtended-Medium", size: 16))
 			#endif
-				
+				Text(commitInfo).opacity(0.5).font(.custom("LoosExtended-Medium", size: 16))
 			}
 			
 				.background(Color(uiColor: .systemBackground))
