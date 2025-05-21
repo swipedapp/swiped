c9bf30284ad3681c45ddbce02d8b8fdd921cb2a2
Adam Demasi <adam@hbang.com.au>
2025-05-21 21:08:35 +0930

    Modified: swiped./BehindView.swift

--- swiped./BehindView.swift
+++ swiped./BehindView.swift
@@ -8,6 +8,30 @@
 import SwiftUI
 import SQLite
 
+struct SummaryGridView: SwiftUI.View {
+
+	let totalKept: Int
+	let totalDeleted: Int
+
+	var body: some SwiftUI.View {
+		let total = totalKept + totalDeleted
+		let keptCount = total == 0 ? 0 : Int(Double(totalKept) / Double(total) * 100)
+
+		return VStack {
+			ForEach(0..<10) { i in
+				HStack {
+					ForEach(0..<10) { j in
+						return Circle()
+							.fill(Color((i * 10) + j < keptCount ? UIColor.sampleGreen : UIColor.sampleRed))
+					}
+				}
+			}
+		}
+			.padding(.vertical, 10)
+	}
+
+}
+
 struct BehindView: SwiftUI.View {
 
 	protocol Delegate: AnyObject {
@@ -21,21 +45,30 @@ struct BehindView: SwiftUI.View {
 	var body: some SwiftUI.View {
 		let db = DatabaseController.shared
 
-		return VStack {
-			Text("\(db.getTotalKept().formatted()) kept")
-			Text("\(db.getTotalDeleted().formatted()) deleted")
-			Text("\(Self.fileSizeFormatter.string(fromByteCount: Int64(db.getSpaceSaved()))) saved")
-			Text("SwipeScore: \(db.calcSwipeScore().formatted())")
+		return VStack(alignment: .leading, spacing: 10) {
+			SummaryGridView(totalKept: db.getTotalKept(), totalDeleted: db.getTotalDeleted())
+
+			VStack(alignment: .leading, spacing: 10) {
+				Text("\(db.getTotalKept().formatted()) kept")
+				Text("\(db.getTotalDeleted().formatted()) deleted")
+				Text("\(Self.fileSizeFormatter.string(fromByteCount: Int64(db.getSpaceSaved()))) saved")
+				Text("SwipeScore: \(db.calcSwipeScore().formatted())")
+			}
+				.font(.custom("LoosExtended-Bold", size: 16))
+				.multilineTextAlignment(.center)
 
 			Button {
 				delegate?.didTapContinue()
 			} label: {
 				Text("Continue")
 			}
-				.foregroundColor(Color("brandGreen"))
+				.frame(maxWidth: .infinity)
+				.font(.custom("LoosExtended-Bold", size: 16))
+				.padding(.vertical, 10)
+				.background(Color("brandGreen").cornerRadius(8))
+				.padding(.top, 10)
 		}
-			.font(.custom("LoosExtended-Bold", size: 16))
-			.multilineTextAlignment(.center)
+			.padding(15)
 	}
 }
 
