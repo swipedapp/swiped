66a2d699ec3ffdc1d55458270e18fc702da8452a
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-05-22 22:51:33 +1000

    Create MigrationUI.swift

---
+++ swiped./MigrationUI.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  swiped.
+//
+//  Created by tobykohlhagen on 22/5/2025.
+//
+
+import SwiftUI
+
+struct MigrationUI: View {
+	var body: some View {
+		NavigationView {
+			VStack {
+				
+				Text("Updating Database")
+				//.font(.custom("LoosExtended-Bold", size: 23))
+					.font(.custom("LoosExtended-Bold", size: 23))
+				Text("Please wait..")
+				//.font(.custom("LoosExtended-Bold", size: 23))
+					.font(.custom("LoosExtended-Medium", size: 16))
+				ProgressView()
+				}
+
+				.scrollContentBackground(.hidden)
+				.background(Color(uiColor: .systemBackground))
+				Spacer()
+			}
+		}
+		//.background(Color(uiColor: .systemBackground))
+	}
+
+#Preview {
+    MigrationUI()
+}
