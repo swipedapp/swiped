423476da4b557b0ca232aae4d6d7c8e744ea648e
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-05-23 13:08:01 +1000

    Modified: swiped.UITests/swiped_UITests.swift

--- swiped.UITests/swiped_UITests.swift
+++ swiped.UITests/swiped_UITests.swift
@@ -18,17 +18,57 @@ final class swiped_UITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+	
+	@MainActor
+	func testStuff() throws {
+		
+	}
+    
 
     @MainActor
-    func testExample() throws {
+			func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+			
+			app.activate()
+			let keepStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Keep"]/*[[".buttons[\"Keep\"].staticTexts.firstMatch",".buttons.staticTexts[\"Keep\"]",".staticTexts[\"Keep\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+			keepStaticText.tap()
+			
+			let deleteStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Delete"]/*[[".buttons[\"Delete\"].staticTexts.firstMatch",".buttons.staticTexts[\"Delete\"]",".staticTexts[\"Delete\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			keepStaticText.tap()
+			deleteStaticText.tap()
+			
+			XCUIDevice.shared.press(.home)
+			
+			let springboardApp = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+			springboardApp/*@START_MENU_TOKEN@*/.buttons["Delete"]/*[[".otherElements.buttons[\"Delete\"]",".buttons[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+			
     }
 	
 	
