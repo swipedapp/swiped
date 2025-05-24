29fe2c47385179e25dc5d3b568c76bea71ed7656
Adam Demasi <adam@hbang.com.au>
2025-05-25 00:19:34 +0930

    Modified: swiped./ImportantInfoView.swift swiped./ViewController.swift

--- swiped./ImportantInfoView.swift
+++ swiped./ImportantInfoView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ImportantInfoView: View {
+
+	@EnvironmentObject var sheetManager: SheetManager
+
+	var json: SettingsJson {
+		return sheetManager.json!
+	}
+
 	var body: some View {
 		NavigationView {
 			VStack {
@@ -23,9 +30,9 @@ struct ImportantInfoView: View {
 					.padding(.top, 17)
 				}
 
-				Image(systemName: "exclamationmark.shield.fill")
+				Image(systemName: json.hasDroppedSupport ? "xmark" : "exclamationmark.shield.fill")
 					.font(.system(size: 80))
-					.foregroundColor(.primary)
+					.foregroundColor(json.hasDroppedSupport ? .brandRed : .primary)
 					.padding([.bottom, .top], 20)
 				Spacer()
 				Text("You're on iOS \(UIDevice.current.systemVersion)")
@@ -34,16 +41,18 @@ struct ImportantInfoView: View {
 					.padding(.bottom, 20)
 				Form {
 					Section {
-						Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe unfortunately will be dropping support for your version of iOS to adapt to the evolution of tomorrows tech. We know it's not ideal, but we recommend you update to a later version of iOS to get the latest updates from this app.")
-						
-						/// or for dead
-						//Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe have discontinued support for your version of iOS. Meaning you will no longer receive quality updates. Please update iOS or switch to a device that is compatible with iOS \(json.minimumiOSVersion) or later.")
-							.listRowBackground(Color(.systemBackground))
-							.font(.custom("LoosExtended-Regular", size: 16))
+						if json.hasDroppedSupport {
+							Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe have discontinued support for your version of iOS. Meaning you will no longer receive quality updates. Please update iOS or switch to a device that is compatible with iOS \(json.minimumiOSVersion) or later.")
+								.listRowBackground(Color(.systemBackground))
+						} else {
+							Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe unfortunately will be dropping support for your version of iOS to adapt to the evolution of tomorrows tech. We know it's not ideal, but we recommend you update to a later version of iOS to get the latest updates from this app.")
+								.listRowBackground(Color(.systemBackground))
+						}
 						
 						
 					}
-					
+					.font(.custom("LoosExtended-Regular", size: 16))
+
 				}
 				.scrollContentBackground(.hidden)
 				.background(Color(uiColor: .systemBackground))
