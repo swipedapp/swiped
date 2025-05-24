bd92fc75bd1e5cf24c93cc02b56d61e8569db6f3
Adam Demasi <adam@hbang.com.au>
2025-05-24 14:08:30 +0930

    Modified: swiped./SwipedApp.swift swiped./swiped..entitlements

--- swiped./SwipedApp.swift
+++ swiped./SwipedApp.swift
@@ -19,7 +19,7 @@ struct SwipedApp: App {
 	init() {
 		do {
 			let config = ModelConfiguration(url: URL.documentsDirectory.appending(path: "swiped-v2.sqlite3"),
-																			cloudKitDatabase: .private("iCloud.com.ma.swipedapp"))
+																			cloudKitDatabase: .private("iCloud.com.ma.swipeddata"))
 			modelContainer = try ModelContainer(for: Photo.self, configurations: config)
 			db.modelContext = modelContainer.mainContext
 		} catch {
