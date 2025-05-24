c739a5d796681acec51d96ec44d67544981df7db
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-05-24 20:49:27 +1000

    Modified: swiped./ImportantInfoView.swift

--- swiped./ImportantInfoView.swift
+++ swiped./ImportantInfoView.swift
@@ -23,9 +23,9 @@ struct ImportantInfoView: View {
 		NavigationView {
 			VStack {
 	
-				Image(systemName: "xmark.shield.fill")
+				Image(systemName: "exclamationmark.shield.fill")
 					.font(.system(size: 80))
-					.foregroundColor(.red)
+					.foregroundColor(.primary)
 					.padding([.bottom, .top], 20)
 				Spacer()
 				Text("You're on iOS 17.0")
@@ -34,10 +34,10 @@ struct ImportantInfoView: View {
 					.padding(.bottom, 20)
 				Form {
 					Section {
-						//Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe unfortunately will be dropping support for your version of iOS to adapt to the evolution of tomorrows tech. We know it's not ideal, but we recommend you update to a later version of iOS to get the latest updates from this app.")
+						Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe unfortunately will be dropping support for your version of iOS to adapt to the evolution of tomorrows tech. We know it's not ideal, but we recommend you update to a later version of iOS to get the latest updates from this app.")
 						
 						/// or for dead
-						Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe have discontinued support for your version of iOS. Meaning you will no longer receive quality updates. Please update iOS or switch to a device that is compatible with iOS 18 or later.")
+						//Text("As we continue to pave the way for the future of this app, we sometimes need new tools. Tools that simply don't exist on your version of iOS.\n\nWe have discontinued support for your version of iOS. Meaning you will no longer receive quality updates. Please update iOS or switch to a device that is compatible with iOS 18 or later.")
 							.listRowBackground(Color(.systemBackground))
 							.font(.custom("LoosExtended-Regular", size: 16))
 						
