4de0657308ce0fdc74e3c6e35057eb99f723a795
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-05-30 17:28:49 +1000

    Added: swiped./AboutView.swift

---
+++ swiped./AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  swiped.
+//
+//  Created by tobykohlhagen on 30/5/2025.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
