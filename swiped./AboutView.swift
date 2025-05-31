4935af2daeb99877c239abdfa4120326b0ebdad2
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-05-31 15:47:11 +1000

    Modified: swiped./AboutView.swift

--- swiped./AboutView.swift
+++ swiped./AboutView.swift
@@ -8,11 +8,144 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State var showRestriction = false
+	let commitInfo = Bundle.main.infoDictionary?["GitCommitHash"] as? String ?? "Unknown"
+	
+	@AppStorage("timestamps")
+	var timestamps: Bool = false
+	
+	var version: String {
+		Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+	}
+	
+	var build: String {
+		Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+	}
+	
+	
+	@EnvironmentObject var sheetManager: SheetManager
+	
+	
+	
+	@State
+	var showResetAlert: Bool = false
+	
+	@AppStorage("swipeDownCount")
+	var swipeDownCount = 0
+	
+	@Environment(\.modelContext) var modelContext
+	
+	var body: some View {
+		NavigationStack {
+			VStack {
+				Form {
+					Section(content: {}, header: {
+						ZStack(alignment: .top) {
+							if swipeDownCount < 5 {
+								HStack {
+									Image(systemName: "chevron.down")
+									Text("Swipe down to dismiss")
+									Image(systemName: "chevron.down")
+								}
+								.font(.custom("LoosExtended-Regular", size: 14))
+							}
+							
+							HStack {
+								Spacer()
+								VStack(alignment: .center) {
+									Text("SWIPED")
+										.foregroundColor(.primary)
+										.font(.custom("LoosExtended-Bold", size: 50))
+									+
+									Text(".")
+										.foregroundColor(Color("brandGreen"))
+										.font(.custom("LoosExtended-Bold", size: 50))
+#if !INTERNAL
+									Text("Version \(version) (\(build))")
+										.font(.custom("LoosExtended-Medium", size: 18))
+#endif
+								}
+								Spacer()
+							}
+							.padding(.vertical, 30)
+						}
+					})
+					
+					
+					// Production flags
+					Toggle(isOn: $timestamps) {
+						
+						Text("Show relative timestamps")
+							.font(.custom("LoosExtended-Regular", size: 16))
+						
+					}.listRowBackground(Color("listRowBackground"))
+					NavigationLink("App Icons") {
+						SettingsIconView(collection: "main")
+					}.listRowBackground(Color("listRowBackground")).font(.custom("LoosExtended-Regular", size: 16))
+					NavigationLink("Advanced") {
+						AdvancedView()
+							.environmentObject(sheetManager)
+					}
+					.font(.custom("LoosExtended-Regular", size: 16))
+					.listRowBackground(Color("listRowBackground"))
+					NavigationLink("About") {
+						AboutView()
+							.environmentObject(sheetManager)
+					}
+					.font(.custom("LoosExtended-Regular", size: 16))
+					.listRowBackground(Color("listRowBackground"))
+					
+					Section {
+						Button(action: {
+							showResetAlert = true
+						}, label: {
+							HStack {
+								Spacer()
+								Label("Reset Database", systemImage: "xmark.bin")
+									.font(.custom("LoosExtended-Medium", size: 16))
+								Spacer()
+							}
+						})
+						.foregroundColor(.red)
+						.listRowBackground(Color("listRowBackground"))
+					}
+				}
+				.scrollContentBackground(.hidden)
+				.background(Color("oled"))
+				.alert("You will lose all statistics you have collected so far. Are you sure you want to do this?", isPresented: $showResetAlert, actions: {
+					Button("Continue", role: .destructive) {
+						Task {
+							let db = DatabaseController(modelContainer: modelContext.container)
+							await db.reset()
+							self.swipeDownCount = 0
+						}
+					}
+					Button("Cancel", role: .cancel) {}
+				})
+				
+				Spacer()
+				
+#if INTERNAL
+				Text("For testing purposes only.").opacity(0.5).font(.custom("LoosExtended-Medium", size: 16))
+#elseif DEBUG
+				Text("Candidate Release").opacity(0.5).font(.custom("LoosExtended-Medium", size: 16))
+#endif
+				Text(commitInfo).opacity(0.5).font(.custom("LoosExtended-Regular", size: 16))
+			}
+			
+			.background(Color("oled"))
+			.navigationBarTitleDisplayMode(.large)
+		}
+		.onAppear {
+			self.swipeDownCount += 1
+		}
+	}
+	
 }
 
+
 #Preview {
-    AboutView()
+	AboutView()
+		.environmentObject(SheetManager())
 }
+
