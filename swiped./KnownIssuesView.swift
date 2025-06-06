dd3da3096df36453f0fa50b6684535f71322bf21
Toby Kohlhagen <tobygkohlhagen@icloud.com>
2025-06-06 13:48:05 +1000

    Modified: swiped./KnownIssuesView.swift

--- swiped./KnownIssuesView.swift
+++ swiped./KnownIssuesView.swift
@@ -8,10 +8,35 @@
 import SwiftUI
 
 struct KnownIssuesView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	var body: some View {
+		NavigationView {
+			VStack {
+				
+				Image(systemName: "xmark.shield.fill")
+					.font(.system(size: 80))
+					.foregroundColor(.primary)
+					.padding([.bottom, .top], 20)
+				Spacer()
+				Text("Known Issues")
+				//.font(.custom("LoosExtended-Bold", size: 23))
+					.font(.custom("LoosExtended-Bold", size: 23))
+					.padding(.bottom, 20)
+				
+				VStack(alignment: .leading) {
+						Text("Hello World")
+					
+					Spacer()
+				}
+				.font(.custom("LoosExtended-Regular", size: 16))
+				.padding(.horizontal, 30)
+				.padding(.vertical, 20)
+			}
+			.background(Color(uiColor: .systemBackground))
+			.navigationBarHidden(true)
+		}
+	}
 }
+
 
 #Preview {
     KnownIssuesView()
